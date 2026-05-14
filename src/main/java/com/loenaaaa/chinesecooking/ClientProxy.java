34f979cac9a9ba9636a7eabba21b52bc7bf33b8f
loenaaaa <asusactiongamer@gmail.com>
2026-05-14 11:39:58 +0800

    Placeholder replacement

--- src/main/java/com/loenaaaa/chinesecooking/ClientProxy.java
+++ src/main/java/com/loenaaaa/chinesecooking/ClientProxy.java
@@ -1,4 +1,4 @@
-package com.myname.mymodid;
+package com.loenaaaa.chinesecooking;
 
 public class ClientProxy extends CommonProxy {
 
--- src/main/java/com/loenaaaa/chinesecooking/Config.java
+++ src/main/java/com/loenaaaa/chinesecooking/Config.java
@@ -1,4 +1,4 @@
-package com.myname.mymodid;
+package com.loenaaaa.chinesecooking;
 
 import java.io.File;
 
